0755069fdadb298c2f5f1a14bd0bdffc4db6b400
david <davidstock004@gmail.com>
2026-09-21 09:53:25 -0600

    Functioning grader

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter a precentage: ");
+        string input = Console.ReadLine();
+        int percentagae = int.Parse(input);
+
+        if (percentagae >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (percentagae >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (percentagae >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (percentagae >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
     }
 }
