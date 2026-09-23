f0b4b5c7e4c429e9c6d555cdefb1c3880a2915f5
david <davidstock004@gmail.com>
2026-09-23 12:46:31 -0600

    User enters number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("Please enter a number ");
+        string input = Console.ReadLine();
+        int number = int.Parse(input);
+        Console.Write("Please enter your guess ");
+        string guessinput = Console.ReadLine();
+        int guess = int.Parse(guessinput);
+
+        if (number > guess)
+            Console.WriteLine("Higher");
+        else if (number < guess)
+            Console.WriteLine("Lower");
+        else 
+            Console.WriteLine("Correct");
     }
 }
