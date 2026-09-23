c2e843bc6f754989ba3736c2a14b87071d1f1d48
david <davidstock004@gmail.com>
2026-09-23 13:10:57 -0600

    Stretch challenges

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,21 +1,47 @@
 using System;
+using System.Reflection.Metadata;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Please enter a number ");
-        string input = Console.ReadLine();
-        int number = int.Parse(input);
-        Console.Write("Please enter your guess ");
-        string guessinput = Console.ReadLine();
-        int guess = int.Parse(guessinput);
+        bool correct = false;
+        bool playAgain = false;
+        
+        int number = Random.Shared.Next(1, 101);
+        int attempts = 0;
+        
+        
+        while (! playAgain)
+        {
+            while (! correct)
+            {
+        
+                Console.Write("Please enter your guess ");
+                string guessinput = Console.ReadLine();
+                int guess = int.Parse(guessinput);
 
-        if (number > guess)
-            Console.WriteLine("Higher");
-        else if (number < guess)
-            Console.WriteLine("Lower");
-        else 
-            Console.WriteLine("Correct");
+                if (number > guess)
+                {
+                    Console.WriteLine("Higher");
+                    attempts += 1;
+                }
+                else if (number < guess)
+                {
+                    Console.WriteLine("Lower");
+                    attempts += 1;
+                }
+                else 
+                {
+                    Console.WriteLine($"Correct, You took {attempts} attempts");
+                    correct = true;
+                }
+            }
+            Console.Write("Would you like to play again? (y/n) ");
+            string again = Console.ReadLine();
+            if (again == "y")
+                playAgain = true;
+        }
     }
 }
